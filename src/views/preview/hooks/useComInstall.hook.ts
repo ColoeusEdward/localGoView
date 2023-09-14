7e64937c88c068929b4c203f0d247a82147a241c
Edward <a50541853@gmail.com>
2023-09-14 14:19:31 +0800

    fix: config install

--- src/views/preview/hooks/useComInstall.hook.ts
+++ src/views/preview/hooks/useComInstall.hook.ts
@@ -1,7 +1,7 @@
 import { ref } from 'vue'
 import { ChartEditStorageType } from '../index.d'
 import { CreateComponentType, CreateComponentGroupType } from '@/packages/index.d'
-import { fetchChartComponent } from '@/packages/index'
+import { fetchChartComponent, fetchConfigComponent } from '@/packages/index'
 
 export const useComInstall = (localStorageInfo: ChartEditStorageType) => {
   const show = ref(false)
@@ -14,6 +14,7 @@ export const useComInstall = (localStorageInfo: ChartEditStorageType) => {
       const intComponent = (target: CreateComponentType) => {
         if (!window['$vue'].component(target.chartConfig.chartKey)) {
           window['$vue'].component(target.chartConfig.chartKey, fetchChartComponent(target.chartConfig))
+          window['$vue'].component(target.chartConfig.conKey, fetchConfigComponent(target.chartConfig))
         }
       }
 
